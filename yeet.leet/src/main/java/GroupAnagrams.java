3f0128f2b647e5de454bd49395347c947c817a92
John French <jfrench1@terpmail.umd.edu>
2019-07-09 10:58:13 -0400

    Group Anagrams

--- yeet.leet/src/main/java/GroupAnagrams.java
+++ yeet.leet/src/main/java/GroupAnagrams.java
@@ -4,20 +4,20 @@ import java.util.stream.Collectors;
 /**
  * Group Anagrams
  * https://leetcode.com/problems/group-anagrams/
- *   Go to Discuss
+ * Go to Discuss
  * Given an array of strings, group anagrams together.
- *
+ * <p>
  * Example:
- *
+ * <p>
  * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
  * Output:
  * [
- *   ["ate","eat","tea"],
- *   ["nat","tan"],
- *   ["bat"]
+ * ["ate","eat","tea"],
+ * ["nat","tan"],
+ * ["bat"]
  * ]
  * Note:
- *
+ * <p>
  * All inputs will be in lowercase.
  * The order of your output does not matter.
  */
@@ -36,16 +36,22 @@ public class GroupAnagrams {
         List<List<String>> lists = new GroupAnagrams().groupAnagrams(strings);
         System.out.println("lists = " + lists);
     }
+
     /**
      * Thoughts are that i can sort the words as a kind of key
      * Group by that key
      * Could be faster if we grouped by a character frequency map instead of sorting words
      * could be faster if we did not use streams
      * Leet Code says that frequency map is slower than sorting the words
-     *
      */
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<Map<Character, Integer>, List<String>> keyToAnagrams = Arrays.stream(strs).collect(Collectors.groupingBy(this::letterFrequency));
+        Map<Map<Character, Integer>, List<String>> keyToAnagrams = Arrays
+                .stream(strs)
+                .collect(
+                        Collectors.groupingBy(
+                                this::letterFrequency
+                        )
+                );
         return new ArrayList<>(keyToAnagrams.values());
     }
 
